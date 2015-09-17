8f807d5b72182394e6d0f81276f0d153ab24602d
JP de Vooght <jp@vooght.de>
2015-09-17 17:50:25 +0200

    added highlights and sample JSON snippets

---
+++ README.md
@@ -0,0 +1,31 @@
+# Alpha Dor
+
+Simple front-end for Dell Doradus. Currently only supports Spider databases. WIP
+
+## Installation
+
+Install on a web server and access from a browser.
+
+## Usage
+
+TODO: Write usage instructions
+
+## Contributing
+
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request :D
+
+## History
+
+TODO: Write history
+
+## Credits
+
+TODO: Write credits
+
+## License
+
+This project is licensed under the terms of the MIT license.
--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
+// spiderBrowser
+
 var sb = {
+  state: '',
   host: '',
   port: '',
   app: '',
@@ -9,52 +12,37 @@ var sb = {
   tables: {},
   field: '',
   fields: {},
-  
-  init: function() {
-    sb.host = 'no host';
-    $('.nav button').text(sb.host);
-    $('#raw').val('');
-    $('[id^=panel] .list-group').find('a').remove();
+
+  setState: function(panel, item) {
+    $('#tab-pane-schema #post').prop('disabled', item !== 'new');
+    $('#tab-pane-schema #delete').prop('disabled', item === 'new');
+    if (panel !== '' && panel !== 'Tenants' && item !== 'new') {
+      $('#tab-data').removeClass('disabled');
+    } else if (!$('#tab-data').hasClass('disabled')) {
+        $('#tab-data').addClass('disabled');
+    }
+    sb.state = panel;
+  },
+ 
+  clearPanelsFrom: function(panel) {
+    switch(panel) {
+      case null:
+      case 'Tenants':
+        $('#panel1 .list-group').find('a').remove();
+      case 'Applications':
+        $('#panel2 .list-group').find('a').remove();
+      case 'Tables':
+        $('#panel3 .list-group').find('a').remove();
+      case 'Fields':
+        $('#panel4 .list-group').find('a').remove();
+        break;
+    }
+    $('#schema-text').val('');
+    $('#data-text').val('');
     $('[id^=panel]').css('height', 'auto');
-    sb.equalize();
-    $('#panel1').on('click', '.list-group-item', function() {
-      var ten = $(this).text();
-      if (sb.tenant !== ten) {
-        sb.tenant = ten;
-        sb.setApps(sb.tenant);
-      } else {
-        $('#tab1 a').text("Tenants");
-        $('#raw').val(JSON.stringify(sb.tenants, null, 2));
-      }
-    });
-    $('#panel2').on('click', '.list-group-item', function() {
-      var app = $(this).text();
-      if (sb.app !== app) {
-        sb.app = app;
-        sb.setTables(sb.app);
-      } else {
-        $('#tab1 a').text("Application");
-        $('#raw').val(JSON.stringify(sb.apps.options, null, 2));
-      }
-    });
-    $('#panel3').on('click', '.list-group-item', function() {
-      var tbl = $(this).text();
-      if (sb.table !== tbl) {
-        sb.table = tbl;
-        sb.setFields(sb.table);
-      } else {
-        $('#tab1 a').text("Table");
-        $('#raw').val('');
-      }
-    });
-    $('#panel4').on('click', '.list-group-item', function() {
-      sb.field = $(this).text();
-      $('#tab1 a').text("Field");
-      $('#raw').val(JSON.stringify(sb.tables[sb.table].fields[sb.field], null, 2));
-    });
   },
 
-  equalize: function() {
+  equalizePannels: function() {
     var mh;
     mh = Math.max.apply(null,
            $('[id^=panel]').map(function() {
@@ -62,6 +50,106 @@ var sb = {
            }).get()
          );
     $('[id^=panel]').css('height', mh);
+  },
+
+  init: function() {
+    sb.clearPanelsFrom('Tenants');
+
+    $('.nav-tabs').on('show.bs.tab', 'li.disabled a', function(e) {
+      e.stopImmediatePropagation();
+      return false;
+    });
+    $('.nav-tabs').off('show.bs.tab', 'li:not(.disabled) a')
+
+    $('#panel1').on('click', '.list-group-item', function() {
+      sb.tenant = $(this).text();
+      $(this).parent().find('a').removeClass('active');
+      $(this).addClass('active');
+      sb.clearPanelsFrom('Applications');
+      if (sb.tenant === 'new') {
+        $('#schema-text').val(JSON.stringify({
+          "HelloKitty": {
+            "users": {
+              "Katniss": {"password": "Everdeen"}
+            },
+            "options": {
+              "ReplicationFactor": "3"
+            }
+          }
+        }, null, 2));
+        sb.equalizePannels();
+      } else {
+        sb.setApps(sb.tenant);
+        $('#schema-text').val(JSON.stringify(sb.tenants, null, 2));
+      }
+      sb.setState('Tenants', sb.tenant);
+    });
+
+    $('#panel2').on('click', '.list-group-item', function() {
+      sb.app = $(this).text();
+      $(this).parent().find('a').removeClass('active');
+      $(this).addClass('active');
+      sb.clearPanelsFrom('Tables');
+      if (sb.app === 'new') {
+        $('#schema-text').val(JSON.stringify({
+          "Msgs": {
+            "key": "MsgsKey",
+            "options": { },
+            "tables": { }
+          }
+        }, null, 2));
+        sb.equalizePannels();
+      } else {
+        sb.setTables(sb.app);
+        $('#schema-text').val(JSON.stringify(
+          sb.apps.options, null, 2)
+        );
+      }
+      sb.setState('Applications', sb.app);
+    });
+
+    $('#panel3').on('click', '.list-group-item', function() {
+      sb.table = $(this).text();
+      $(this).parent().find('a').removeClass('active');
+      $(this).addClass('active');
+      sb.clearPanelsFrom('Fields');
+      if (sb.table === 'new') {
+        $('#schema-text').val(JSON.stringify({
+          "tables": {
+            "Message": {
+              "fields": { },
+              "aliases": { }
+            }
+          }
+        }, null, 2));
+        sb.equalizePannels();
+      } else {
+        sb.setFields(sb.table);
+      }
+      sb.setState('Tables', sb.table);
+    });
+
+    $('#panel4').on('click', '.list-group-item', function() {
+      sb.field = $(this).text();
+      $(this).parent().find('a').removeClass('active');
+      $(this).addClass('active');
+      if (sb.field === 'new') {
+        $('#schema-text').val(JSON.stringify({
+          "fields": {
+            "DirectReports": {
+              "table": sb.table,
+              "type": "",
+              "analyzer": ""
+            }
+          }
+        }, null, 2));
+      } else {
+        $('#schema-text').val(JSON.stringify(
+          sb.tables[sb.table].fields[sb.field], null, 2)
+        );
+      }
+      sb.setState('Fields', sb.field);
+    });
   },
 
   setTenants: function(host, port) {
@@ -80,31 +168,30 @@ var sb = {
         sb.tables = {};
         sb.field = '';
         $('.nav button').text(host);
-        $('#tab1 a').text("Tenants");
-        $('#raw').val(JSON.stringify(sb.tenants, null, 2));
-        $('[id^=panel] .list-group').find('a').remove();
-        $('[id^=panel]').css('height', 'auto');
+        $('#schema-text').val(JSON.stringify(sb.tenants, null, 2));
         for(ten in sb.tenants) {
           $('#panel1 ul.list-group').append(
             '<a href="#" class="list-group-item">'+ten+'</a>'
           );
         };
-        sb.equalize();
+        $('#panel1 ul.list-group').append(
+          '<a href="#" class="list-group-item">new</a>'
+        );
+        sb.equalizePannels();
       }
     });
   },
 
   setApps: function(tenant) {
-    $('#panel2 .list-group').find('a').remove();
-    $('#panel3 .list-group').find('a').remove();
-    $('#panel4 .list-group').find('a').remove();
-    $('[id^=panel]').css('height', 'auto');
     sb.tenants[tenant].applications.map(function(item) {
       $('#panel2 ul.list-group').append(
         '<a href="#" class="list-group-item">'+item+'</a>'
       );
     });
-    sb.equalize();
+    $('#panel2 ul.list-group').append(
+      '<a href="#" class="list-group-item">new</a>'
+    );
+    sb.equalizePannels();
   },
 
   setTables: function(app) {
@@ -115,59 +202,56 @@ var sb = {
       success: function(res) {
         sb.apps = res[app];
         sb.tables = res[app].tables;
-        $('#tab1 a').text("Application");
-        $('#raw').val(JSON.stringify(sb.apps, null, 2));
-        $('#panel3 .list-group').find('a').remove();
-        $('#panel4 .list-group').find('a').remove();
-        $('[id^=panel]').css('height', 'auto');
+        $('#schema-text').val(JSON.stringify(sb.apps, null, 2));
         Object.keys(sb.tables).map(function(item) {
           $('#panel3 ul.list-group').append(
             '<a href="#" class="list-group-item">'+item+'</a>'
           );
         });
-        sb.equalize();
+        $('#panel3 ul.list-group').append(
+          '<a href="#" class="list-group-item">new</a>'
+        );
+        sb.equalizePannels();
       }
     });
   },
 
   setFields: function(table) {
-    $('#panel4 .list-group').find('a').remove();
-    $('[id^=panel]').css('height', 'auto');
     var fields = sb.tables[table].fields;
     Object.keys(fields).map(function(item) {
       $('#panel4 ul.list-group').append(
         '<a href="#" class="list-group-item">'+item+'</a>'
       );
     });
-    sb.equalize();
+    $('#panel4 ul.list-group').append(
+      '<a href="#" class="list-group-item">new</a>'
+    );
+    sb.equalizePannels();
   }
 };
 
 var app = {
   setupAjaxCallbacks: function() {
     $(document).ajaxStart(function() {
-      $('#flashMsg').show().text("Loading...");
+      $('#flashMsg').text('loading...').show();
     });
 
     $(document).ajaxStop(function() {
-      $('#flashMsg').delay(1000).fadeOut("slow");
+      $('#flashMsg').delay(500).fadeOut(500);
     });
 
-    $(document).ajaxError(
-      function(event, xhr, opts, err) {
-        if (xhr.status === 401) {
-        }
-        $('#flashMsg').show().text(
-          xhr.statusText.toLowerCase()
-        );
-        console.log(JSON.stringify(xhr));
-      });
+    $(document).ajaxError(function(event, xhr, opts, err) {
+      $('#flashMsg').html('<span class="label label-danger">Error</span>&nbsp;&nbsp;'+xhr.statusText.toLowerCase()).show();
+      console.log(JSON.stringify(xhr));
+    });
   },
 
   setupNavbar: function() {
+    $('.nav button').text('no host');
     $('#set_host').click(function() {
       var host = $('#hostname').val();
       var port = $('#port').val();
+      sb.clearPanelsFrom('Tenants');
       sb.setTenants(host, port);
     });
   }
